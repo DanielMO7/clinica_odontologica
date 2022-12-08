57accfd55136956b01d08c68cadc0f54a211bb02
DanielMO7 <dani019jorje@gmail.com>
2022-12-07 22:38:34 -0500

    Agregada validacion del documento y el email

--- db-users.php
+++ db-users.php
@@ -1,26 +1,77 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-	<title>db-usuarios</title>
-	<meta charset="utf-8">
+    <title>db-usuarios</title>
+    <meta charset="utf-8">
 </head>
+
 <body>
     <!--MD5 enripta las contraseñas-->
-<?php 
+    <?php
 
-	$conexion = mysqli_connect("localhost:3306", "root", "", "odontomax") or die ("Problemas con la conexion");
+    // Encripta la contraseña
+    $password_bycript = password_hash("$_REQUEST[password]", PASSWORD_BCRYPT);
 
-	mysqli_query($conexion, "insert into usuarios(nombre, email, num_tel, identificacion, edad, password, tratamiento) values('$_REQUEST[nombre]', '$_REQUEST[email]', '$_REQUEST[num_tel]', '$_REQUEST[identificacion]', '$_REQUEST[edad]', '$_REQUEST [password]', '$_REQUEST[tratamiento]')")or die("echo <script>
-                alert('Parece que has ingresado informacion invalida. Por favor llena todos los campos.');
-                window.locatio= 'registro.php'
-    </script>" .mysqli_error($conexion));
 
-	mysqli_close($conexion);
-	
-	echo "<script>
-                alert('Te has registrado satisfactoriamente, solo queda loguearte.');
-                window.location= 'login.php'
-    </script>";
- ?>
+    $conexion = mysqli_connect("localhost:3306", "root", "My-pruebas_74", "odontomax") or die("Problemas con la conexion");
+
+    $validar_correo_documento = mysqli_query($conexion, "SELECT * FROM usuarios");
+    $validacion_email = true;
+    $validacion_documento = true;
+    // Valida que el documento y el email no esten repetidos.
+    foreach ($validar_correo_documento as $datos) {
+        if ($datos['email'] == $_REQUEST['email']) {
+            $validacion_email = false;
+        }
+    }
+    foreach ($validar_correo_documento as $datos) {
+        if ($datos['identificacion'] == $_REQUEST['identificacion']) {
+            $validacion_documento = false;
+        }
+    }
+
+    if ($validacion_documento && $validacion_email) {
+        mysqli_query(
+            $conexion,
+            "INSERT INTO usuarios
+                (
+                    nombre, 
+                    email, 
+                    num_tel, 
+                    identificacion, 
+                    edad, 
+                    password, 
+                    tratamiento
+                )
+                VALUES
+                    (
+                        '$_REQUEST[nombre]', 
+                        '$_REQUEST[email]', 
+                        '$_REQUEST[num_tel]', 
+                        '$_REQUEST[identificacion]', 
+                        '$_REQUEST[edad]', 
+                        '$password_bycript', 
+                        '$_REQUEST[tratamiento]'
+                    )"
+        ) or die("echo <script>
+                        alert('Ah ocurrido un error con la base de datos!');
+                        window.history.back()
+                        </script>" . mysqli_error($conexion));
+        mysqli_close($conexion);
+    } else {
+        echo "<script>
+            alert('El e-mail o identificacion ya existen! Por favor, cambialos.');
+            window.history.back()
+        </script>";
+    }
+    if ($validacion_documento && $validacion_email) {
+        echo "<script>
+                    alert('Te has registrado satisfactoriamente, solo queda loguearte.');
+                    window.location= 'login.php'
+        </script>";
+    }
+    ?>
 </body>
+
 </html>
