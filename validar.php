c222a3721d3bd0ec3590fbd6de3b0fb432766cb9
DanielMO7 <dani019jorje@gmail.com>
2022-12-07 22:40:22 -0500

    Agregando validacion de contraseña haseada

--- validar.php
+++ validar.php
@@ -1,25 +1,25 @@
-<?php 
+<?php
 
-	session_start();
+session_start();
 
-	$usuario = $_POST['email'];
-	$pass = $_POST['password'];
+$usuario = $_POST['email'];
+$pass = $_POST['password'];
 
-	$conexion = new mysqli("localhost:3306", "root", "", "odontomax");
+$conexion = new mysqli("localhost:3306", "root", "My-pruebas_74", "odontomax");
 
-	$proceso = mysqli_query($conexion, "SELECT * FROM usuarios WHERE email = '$usuario' AND password = MD5('$pass')")or die("Problemas en el select: ".mysql_error($conexion));
+$proceso = mysqli_query($conexion, "SELECT * FROM usuarios WHERE email = '$usuario'") or die("Problemas en el select: " . mysqli_error($conexion));
 
-	if ($resultado = mysqli_fetch_array($proceso)) {
+foreach ($proceso as $credenciales) {
+	if (password_verify($pass, $credenciales['password'])) {
 		$_SESSION['u_usuario'] = $usuario;
 		echo "<script>
                 alert('Te has logueado exitosamente.');
                 window.location= 'index.php'
     		  </script>";
-	}
-	else{
+	} else {
 		echo "<script>
-                alert('Lo sentimos, tu correo o contraseña son incorrectos. Puede que no estes registrado.');
+                alert('Correo o contraseña incorrectos!');
                 window.location= 'login.php'
     		  </script>";
 	}
- ?>
+}
